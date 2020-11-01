c28d0437ac427522227772994493ea4228ec030b
himeems <michelle@michelles-mbp.home>
2020-11-01 10:08:46 -0500

    Updated weather app

--- src/Weather.js
+++ src/Weather.js
@@ -1,29 +1,82 @@
 import React from "react";
-import axios from "axios";
-import Loader from "react-loader-spinner";
+import "./Weather.css";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `the weather in ${response.data.name} is ${Math.round(
-        response.data.main.temp
-      )} °C`
-    );
-  }
-
-  let units = `metric`;
-  let apiKey = `6e655c51885a817c91e42c6e1aa56edf`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
-
-  axios.get(apiUrl).then(handleResponse);
+export default function Weather() {
+  let weatherData = {
+    city: "New York",
+    condition: "cloudy",
+    date: "sep",
+    description: "partly cloudy icon",
+    humidity: 24,
+    imgUrl: "http://openweathermap.org/img/wn/10d@2x.png",
+    temp: 24,
+    today: "mon 9.28",
+  };
 
   return (
-    <Loader
-      type="Puff"
-      color="#00BFFF"
-      height={100}
-      width={100}
-      timeout={3000}
-    />
+    <div className="Weather">
+      <div className="container">
+        <div className="weather-app">
+          <h1>Weather Now or Later</h1>
+          <form>
+            <input
+              type="search"
+              placeholder="enter city here"
+              className="search-button"
+            />
+            <input type="submit" value="search" className="btn btn-info" />
+            <button className="btn btn-success" w-100>
+              current
+            </button>
+          </form>
+          <div className="row">
+            <div className="col-4">
+              <ul className="today">
+                <li>{weatherData.city}</li>
+                <li>{weatherData.today}</li>
+                <li>{weatherData.date}</li>
+              </ul>
+            </div>
+            <div className="col-4">
+              <div className="clearfix new-temp">
+                <div className="float-left">{weatherData.temp}</div>
+                <span className="float-right">
+                  <a href="/">°C</a> |
+                  <a href="/">
+                    <strong>°F</strong>
+                  </a>
+                </span>
+              </div>
+            </div>
+            <div className="col-4">
+              <img
+                src={weatherData.imgUrl}
+                alt={weatherData.description}
+                className="image-icon"
+              />
+              <br />
+            </div>
+          </div>
+
+          <ul className="update">
+            <hr />
+            <li>condition:</li>
+            <li>humidity: %</li>
+
+            <hr />
+          </ul>
+
+          <div className="row weather-forecast"></div>
+        </div>
+      </div>
+      <small>
+        <a href="https://github.com/hiimeems/shecodes-weather-app">
+          <strong>
+            <i>open source code </i>
+          </strong>
+        </a>
+        by michelle chung
+      </small>
+    </div>
   );
 }
